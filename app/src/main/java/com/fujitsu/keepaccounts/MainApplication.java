fdc378754520cb7c91bb5b4a7ed434f7b15d401a
lw10565fu <474180236@qq.com>
2016-08-26 09:14:57 +0800

    Initial commit

---
+++ app/src/main/java/com/fujitsu/keepaccounts/MainApplication.java
@@ -0,0 +1,11 @@
+package com.fujitsu.keepaccounts;
+
+import android.app.Application;
+
+/**
+ * Created by lw on 2016/8/25.
+ * app全局管理类
+ */
+public class MainApplication extends Application{
+
+}
